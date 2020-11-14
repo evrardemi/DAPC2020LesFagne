f9e26241c234d56d2b3ef3731e13a2825c686461
Birken-coder <david.lannoo.dl@gmail.com>
2020-11-14 12:48:43 +0100

    Birken-coder

--- src/Code/java/P4.java
+++ src/Code/java/P4.java
@@ -1,2 +1,7 @@
+import java.util.Scanner;
+
 public class P4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
--- src/Code/java/P9.java
+++ src/Code/java/P9.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class P9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
