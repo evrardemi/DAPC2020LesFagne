f0b155d33b6acacb1c51ef18a1eac33497bc0496
Eliot <eliot.peeters@student.uclouvain.be>
2020-11-14 11:29:18 +0100

    guesser version 2

--- src/Code/java/Guesser.java
+++ src/Code/java/Guesser.java
@@ -1,20 +1,23 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Main{
-    public int = 500;
-    public Scanner scanner = new Scanner(System.in);
-
-    public static int guess(int previous_number){
+class guesser{
+    public static int guess(int previous_number, Scanenr scanner){
         System.out.printf(previous_number);
         String answer = scanner.nextline();
         switch (answer){
             case "That is correct!" :
                 return previous_number;
             case "Your guess is too high.":
-                guess(previous_number-1);
+                guess(previous_number-1, scanner);
             case "“Your guess is too low.":
-                guess(previous_number+1);
+                guess(previous_number+1, scanner);
         }
+    }
+
+    public static void main(String[] args) {
+        public int number= 500;
+        Scanner scanner = new Scanner(System.in);
+        guess(number, scanner);
     }
 }
